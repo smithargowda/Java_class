a28087123a621896b0b4e67b3d4ccb8c9f2feab7
smithargowda <smithargowda31@gmail.com>
2023-04-30 19:37:17 +0530

    day01
    program01- datatypes

---
+++ src/day01/Program01.java
@@ -0,0 +1,33 @@
+
+package day01;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		boolean a=true;
+		System.out.println(a);
+		a=false;
+		System.out.println(a);
+		byte a1=16;
+		System.out.println(a1);
+		short a2=123;
+		System.out.println(a2);
+		int a3=123456789;
+		System.out.println(a3);
+		a3 =a2;
+		System.out.println(a3);
+		long a4 =1223556799;
+		System.out.println(a4);
+		a4=123456789987456321l;
+		System.out.println(a4);
+		float a6=12346.569f;
+		System.out.println(a6);
+		double a7=123456789.2369852147d;
+		System.out.println(a7);
+		char a8='b';
+		System.out.println(a8);
+				
+
+	}
+
+}
